35c985929bb4188b114b3d626b92a2e802df95ef
felipetv <ft.redprince@gmail.com>
2018-09-03 11:12:55 -0300

    welcome message test

---
+++ src/com/twu/biblioteca/Librarian.java
@@ -0,0 +1,10 @@
+package com.twu.biblioteca;
+
+public class Librarian {
+
+    public static final String WELCOME_TO_BIBLIOTECA = "Welcome to Biblioteca";
+
+    public void sendWelcome() {
+        System.out.println(WELCOME_TO_BIBLIOTECA);
+    }
+}
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -2,12 +2,41 @@ package com.twu.biblioteca;
 
 
 import org.junit.Test;
+
+import java.io.*;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
+
+    public static final String WELCOME_TO_BIBLIOTECA = "Welcome to Biblioteca";
+    public static final String SPACE = "\n";
 
     @Test
     public void test() {
         assertEquals(1, 1);
     }
+
+
+    @Test
+    public void WelcomeMessage() {
+
+
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+
+        Librarian l = new Librarian();
+
+
+        l.sendWelcome();
+
+
+        String salida = outContent.toString();
+        assertEquals(WELCOME_TO_BIBLIOTECA + SPACE, salida);
+
+
+
+    }
+
 }
