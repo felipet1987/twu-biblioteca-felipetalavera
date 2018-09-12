dd3b49c3f15ceaeab8ab55be8daf9b551ef885e8
felipetv <ft.redprince@gmail.com>
2018-09-12 10:49:00 -0300

    recover from fail login test on user service

---
+++ src/com/twu/biblioteca/ListUserService.java
@@ -0,0 +1,27 @@
+package com.twu.biblioteca;
+
+import com.twu.biblioteca.model.User;
+
+public class ListUserService implements UserService {
+    private final UserRepository repo;
+    private User loggedUser;
+
+    public ListUserService(UserRepository repo) {
+        this.repo = repo;
+    }
+
+    @Override
+    public boolean login(String number, String password) {
+
+        if(repo.login(number,password)){
+            this.loggedUser = repo.findByNumber(number);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public User getLoggedUser() {
+        return this.loggedUser;
+    }
+}
--- test/com/twu/biblioteca/UserServiceTest.java
+++ test/com/twu/biblioteca/UserServiceTest.java
@@ -12,7 +12,7 @@ public class UserServiceTest {
         UserRepository repo = new FakeUserRepository();
         UserService service = new ListUserService(repo);
 
-        service.login("123-4567","password");
+        assertEquals(true,service.login("123-4567","password"));
         User u = service.getLoggedUser();
         assertEquals("123-4567",u.getNumber());
         assertEquals("Felipe",u.getName());
