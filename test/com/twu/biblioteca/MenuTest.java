53e94715f1d97065f7c0cbee11ee0598057a75a0
felipetv <ft.redprince@gmail.com>
2018-09-12 08:45:11 -0300

    from last commit

--- test/com/twu/biblioteca/MenuTest.java
+++ test/com/twu/biblioteca/MenuTest.java
@@ -1,8 +1,10 @@
 package com.twu.biblioteca;
 
+import com.twu.biblioteca.model.User;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -10,16 +12,63 @@ import static org.junit.Assert.assertEquals;
 public class MenuTest {
 
     OutputPort out;
+    InputPort in;
     AppMenu menu;
+    UserService userService;
+    BookService bookService;
 
     @Before
     public void setUp() {
         out = new TestOutput();
-        InputPort in = new TestInput();
-        menu = new ListMenu(in, out);
+        in = new TestInput();
+        userService = new FakeUserService();
+        bookService = new FakeBookService();
+        menu = new ListMenu(in, out,userService,bookService);
+
+
 
     }
 
+    @Test
+    public void succesfulLogin() {
+        List<String> data = new ArrayList<>();
+        data.add("123-4567");
+        data.add("password");
+        in.setInput(data);
+
+        menu.login();
+        List<String> stream = out.getOutput();
+        assertEquals("enter library number", stream.get(0));
+        assertEquals("enter password", stream.get(1));
+        assertEquals("Welcome", stream.get(2));
+        
+    }
+
+    @Test
+    public void ShowBookMenu() {
+        menu.showBookMenu();
+
+        List<String> stream = out.getOutput();
+        assertEquals("ID:NAME:AUTHOR:YEAR", stream.get(0));
+        assertEquals("1:name:author:2000", stream.get(1));
+        assertEquals("0 . return book", stream.get(2));
+        assertEquals("1 . checkout book", stream.get(3));
+
+    }
+
+    @Test
+    public void gettingUserInput() {
+
+        List<String> data = new ArrayList<>();
+        data.add("1");
+        in.setInput(data);
+        String option = menu.waitForUser();
+        List<String> stream = out.getOutput();
+        assertEquals("please enter an option", stream.get(0));
+        assertEquals("1",option);
+        
+
+    }
 
     @Test
     public void WelcomeMessage() {
@@ -27,6 +76,7 @@ public class MenuTest {
         List<String> stream = out.getOutput();
         assertEquals("Welcome", stream.get(0));
     }
+
 
 
 }
