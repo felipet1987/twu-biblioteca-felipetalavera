391b76bf3fc7cdba321f1a8d0f8a55e1089fd27f
felipetv <ft.redprince@gmail.com>
2018-09-12 19:08:16 -0300

    messages to globals

--- src/com/twu/biblioteca/globals.java
+++ src/com/twu/biblioteca/globals.java
@@ -1,4 +1,40 @@
 package com.twu.biblioteca;
 
 public class globals {
+
+    public static final String SHOW_USER_DETAILS = "show user details";
+    public static final String SHOW_BOOK_MENU = "show book menu";
+    public static final String SHOW_MOVIE_MENU = "show movie menu";
+    public static final String WELCOME = "Welcome";
+    public static final String EXIT_APP = "exit app";
+    public static final String ENTER_LIBRARY_NUMBER = "enter library number";
+    public static final String ENTER_PASSWORD = "enter password";
+    public static final String ID_NAME_AUTHOR_YEAR = "ID:NAME:AUTHOR:YEAR";
+    public static final String PLEASE_ENTER_AN_OPTION = "please enter an option";
+    public static final String INVALID_OPTION = "Invalid Option";
+
+
+    public static final String PLEASE_ENTER_NAME_OF_BOOK = "please enter name of book";
+
+    public static final String PLEASE_ENTER_NAME_OF_MOVIE = "please enter name of movie";
+
+
+    public static final String MOVIE_HEADER = "ID:NAME:YEAR:DIRECTOR:RATING";
+    public static final String RETURN_MOVIE = "return movie";
+    public static final String CHECKOUT_MOVIE = "checkout movie";
+
+    public static final String RETURN_BOOK = "return book";
+    public static final String CHECKOUT_BOOK = "checkout book";
+
+    public static final String THAT_IS_NOT_A_VALID_MOVIE_TO_RETURN = "That is not a valid movie to return.";
+    public static final String THANK_YOU_FOR_RETURNING_THE_MOVIE = "Thank you for returning the movie.";
+    public static final String THANK_YOU_ENJOY_THE_MOVIE = "Thank you! Enjoy the movie";
+    public static final String THAT_MOVIE_IS_NOT_AVAILABLE = "That movie is not available.";
+
+    public static final String THANK_YOU_ENJOY_THE_BOOK = "Thank you! Enjoy the book";
+    public static final String THAT_BOOK_IS_NOT_AVAILABLE = "That book is not available.";
+    public static final String THANK_YOU_FOR_RETURNING_THE_BOOK = "Thank you for returning the book.";
+    public static final String THAT_IS_NOT_A_VALID_BOOK_TO_RETURN = "That is not a valid book to return.";
+
+
 }
