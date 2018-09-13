a258fb5f52391d5b15a914910c4bc1540087e3d0
felipetv <ft.redprince@gmail.com>
2018-09-13 17:28:16 -0300

    cleaning main

--- src/com/twu/biblioteca/app/BibliotecaApp.java
+++ src/com/twu/biblioteca/app/BibliotecaApp.java
@@ -1,12 +1,41 @@
 package com.twu.biblioteca.app;
 
 
+import com.twu.biblioteca.*;
+import com.twu.biblioteca.io.ConsoleInput;
+import com.twu.biblioteca.io.ConsoleOutput;
+import com.twu.biblioteca.repository.MemoryBookRepository;
+import com.twu.biblioteca.repository.MemoryMovieRepository;
+import com.twu.biblioteca.repository.MemoryUserRepository;
+import com.twu.biblioteca.service.ListBookService;
+import com.twu.biblioteca.service.ListMovieService;
+import com.twu.biblioteca.service.ListUserService;
 
 public class BibliotecaApp {
 
     public static void main(String[] args) {
 
+        AppMenu menu = setup();
+
+        MainController controller = new MainController(menu);
 
 
+
+
+        controller.start();
+        while (!controller.isExited()) {
+            controller.nextOption();
+        }
+
+
+    }
+
+    private static AppMenu setup() {
+        InputPort in = new ConsoleInput();
+        OutputPort out = new ConsoleOutput();
+        UserService userService = new ListUserService(new MemoryUserRepository());
+        BookService bookService = new ListBookService(new MemoryBookRepository());
+        MovieService movieService = new ListMovieService(new MemoryMovieRepository());
+        return new ListMenu(in, out, userService, bookService, movieService);
     }
 }
